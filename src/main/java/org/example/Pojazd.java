04462fa3f7319dd06946195ca2ebf16bee049c59
Paulina Pionk <paulinapionk@gmail.com>
2022-11-26 16:30:44 +0100

    Pojazd

---
+++ src/main/java/org/example/Pojazd.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Pojazd {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String marka;
+    private double moc;
+    private String kolor;
+    private int rokProdukcji;
+    private boolean czyElektryczny;
+
+}
